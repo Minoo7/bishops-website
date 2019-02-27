6e57399cac1e6b1a51097ca5bac5aba7b29aded9
Minoo7 <accounts@minoo.pro>
2019-02-27 17:42:37 +0100

    2019_02_27

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,19 +73,3 @@ function search() {
       }
   }
 }
-
-$(window).load(function(){
-    
-  $(window).scroll(function() {
-      var hT = $('#scroll-to').offset().top,
-      hH = $('#scroll-to').outerHeight(),
-      wH = $(window).height(),
-      wS = $(this).scrollTop();
-      if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
-          $('#static-bar').addClass('view2')
-      }
-      else {
-          $('#static-bar').removeClass('view2');
-      }
-  });
-});
