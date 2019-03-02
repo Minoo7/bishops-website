af0bf723d105bd070720cf08f778e33b81300674
Minoo7 <accounts@minoo.pro>
2019-03-02 11:53:22 +0100

    2019_03_02

--- assets/js/jobb.js
+++ assets/js/jobb.js
@@ -1,14 +1,74 @@
 $(document).ready(function(){
-    
-    $('li').click(function(){
-        $('p').toggle();
-    });
+  $('li').click(function(){
+
+    var tP = $(this).next('ul');
+
+    $('ul').not(tP).hide();
+    $('li').not(this).removeClass('bRed');
+    $('li').not(this).removeClass('padB');
+    $('ul').not(tP).removeClass('bRed');
+
+    tP.toggle();
+    $(this).toggleClass('bRed');
+    $(this).toggleClass('padB');
+    tP.toggleClass('bRed');
+
+    /*document.addEventListener('mousedown', function (event) { //prevent double click selection
+      if (event.detail > 1) {
+        event.preventDefault();
+      }
+    }, false);*/
+  });
 });
 
-/*function showP() {
-  $('p').toggle();
-  //$(this).css({ background: "linear-gradient(to right, rgba(196,22,24,0) 0%, rgba(196, 22, 24, 0.5) 25%, rgba(196,22,24,0) 0%, rgba(196,22,24,0.5) 0%, rgba(196,22,24,0.5) 75%, rgba(196,22,24,0) 100%);" });
-  //$(this).toggle();
-  var ok = $(this)
-  alert(ok);
+$(function() {
+  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+});
+
+/*jQuery.fn.mouseIsOver = function lag() {
+  if($(this[0]).is(":hover"))
+  {
+      return true;
+  }
+  return false;
+}; 
+
+setInterval(function lag(){
+  var $sample = $('li');
+  var $par = $('p');
+
+if($par.mouseIsOver()) {
+     $sample.addClass('nok')
+  }
+  else {
+     $sample.removeClass('nok')
+  }
+}, 200);*/
+
+/*jQuery.fn.mouseIsOver = function () {
+  if($(this[0]).is(":hover"))
+  {
+      return true;
+  }
+  return false;
+}; 
+
+setInterval(function(){
+  var $sample = $("#sample");
+  if($sample.mouseIsOver()) {
+     $sample.css("background", "yellow");
+  }
+  else {
+     $sample.css("background", "");
+  }
+}, 200);*/
+
+/*document.addEventListener('mousedown', function (event) { //prevent double click selection
+  if (event.detail > 1) {
+    event.preventDefault();
+  }
+}, false);*/
+
+/*function kaka() {
+  $('li').addClass('noclick');
 }*/
