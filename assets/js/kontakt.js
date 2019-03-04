700ca0cec5f96e51c011cefa190035c05f72bfc7
minoo7 <accounts@minoo.pro>
2019-03-04 18:55:20 +0100

    2019

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -1,51 +1,72 @@
-function showN() {
-    $('.numberCon i').toggleClass('arrowdown');
-    
-    var delayInMilliseconds = 1000; //1 second
+jQuery(document).on('click','html,body,#main',function(event){
+    event.preventDefault();
+    event.stopPropagation();   
+});
 
-    /*function fadeOn() {
-        i++;
-        var ul = $('.numberCon ul').eq(i)
-        setTimeout(function () {
-            ul.addClass('fullOpacity');
-          }, 1500);
-        // ul.addClass('fullOpacity');
-        console.log($('.numberCon ul:last-child').css('opacity'))
-        if ($('.numberCon ul:last-child').css('opacity') > 0) {
-            clearInterval(timerOn);
+$(document).ready(function(){
+    $('.numberCon h3').click(function showN(){
+        if ($('.numberCon ul').eq(0).css('display') == 'none') {     
+            $('.numberCon i').addClass('arrowdown'); 
+            $(this).off('click', showN);
+            $('.numberCon h3').addClass('wait');
+
+            var d = 0, f = 0;
+            var timerDispOn = setInterval(displayOn, 10);
+            var timerFadeOn = setInterval(fadeOn, 20);
         }
-    }*/
 
-    setTimeout(function fadeOn() {
-        i++;
-        var ul = $('.numberCon ul').eq(i)
-        ul.addClass('fullOpacity');
-        // ul.addClass('fullOpacity');
-        console.log($('.numberCon ul:last-child').css('opacity'))
-        /*if ($('.numberCon ul:last-child').css('opacity') > 0) {
-            clearInterval(timerOn);
-        }*/
-    }, 500);
-
-    fadeOn();
-
-    /*function fadeOff() {
-        i--;
-        var ul = $('.numberCon ul').eq(i)
-        ul.removeClass('fullOpacity');
-        console.log($('.numberCon ul:last-child').css('opacity'))
-        if ($('.numberCon ul:first-child').css('opacity') == 0) {
-            clearInterval(timerOff);
+        if ($('.numberCon ul').eq(0).css('display') == 'block') {
+            $('.numberCon i').removeClass('arrowdown');
+            $(this).off('click', showN);
+            $('.numberCon h3').addClass('wait');
+            
+            var d = $('.numberCon ul').length, f = $('.numberCon ul').length;
+            var timerFadeOff = setInterval(fadeOff, 10);
+            var timerDispOff = setInterval(displayOff, 20);
         }
-    }
 
-    /*if ($('.numberCon ul:last-child').css('opacity') == 0) {
-        var i = -1;
-        var timerOn = setInterval(fadeOn, 100);
-    }
-    else {
-        //var i = $('.numberCon ul').length
-        var i = 7;
-        var timerOff = setInterval(fadeOff, 100);
-    }*/
-}
+        function fadeOn() {
+            var ul_f = $('.numberCon ul').eq(f);     
+            ul_f.addClass('fullOpacity');
+            f++;
+
+            if ($('.numberCon ul:last-child').css('opacity') == 1) {
+                $('.numberCon h3').on('click', showN);
+                $('.numberCon h3').removeClass('wait');
+                clearInterval(timerFadeOn);
+            }
+        }
+
+        function fadeOff() {
+            var ul_f = $('.numberCon ul').eq(f)
+            ul_f.removeClass('fullOpacity');
+            f--;
+
+            if ($('.numberCon ul').eq(0).css('opacity') == 0) {
+                clearInterval(timerFadeOff);
+            }
+        }
+
+        function displayOn() {
+            var ul_d = $('.numberCon ul').eq(d);
+            ul_d.addClass('show');
+            d++;
+
+            if ($('.numberCon ul:last-child').css('display') == 'block') {
+                clearInterval(timerDispOn);
+            }    
+        }
+
+        function displayOff() {
+            var ul_d = $('.numberCon ul').eq(d);
+            ul_d.removeClass('show');
+            d--;
+            
+            if ($('.numberCon ul').eq(0).css('display') == 'none') {
+                $('.numberCon h3').on('click', showN);
+                $('.numberCon h3').removeClass('wait');
+                clearInterval(timerDispOff);
+            }
+        }
+    });
+});
