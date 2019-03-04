5090121e3583801ff8dd801c35cf5cf67bb8d384
minoo7 <accounts@minoo.pro>
2019-03-04 03:09:52 +0100

    2019_03_04

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -1,3 +1,51 @@
-function showd() {
-    $('.numbers ul').addClass('sd');
+function showN() {
+    $('.numberCon i').toggleClass('arrowdown');
+    
+    var delayInMilliseconds = 1000; //1 second
+
+    /*function fadeOn() {
+        i++;
+        var ul = $('.numberCon ul').eq(i)
+        setTimeout(function () {
+            ul.addClass('fullOpacity');
+          }, 1500);
+        // ul.addClass('fullOpacity');
+        console.log($('.numberCon ul:last-child').css('opacity'))
+        if ($('.numberCon ul:last-child').css('opacity') > 0) {
+            clearInterval(timerOn);
+        }
+    }*/
+
+    setTimeout(function fadeOn() {
+        i++;
+        var ul = $('.numberCon ul').eq(i)
+        ul.addClass('fullOpacity');
+        // ul.addClass('fullOpacity');
+        console.log($('.numberCon ul:last-child').css('opacity'))
+        /*if ($('.numberCon ul:last-child').css('opacity') > 0) {
+            clearInterval(timerOn);
+        }*/
+    }, 500);
+
+    fadeOn();
+
+    /*function fadeOff() {
+        i--;
+        var ul = $('.numberCon ul').eq(i)
+        ul.removeClass('fullOpacity');
+        console.log($('.numberCon ul:last-child').css('opacity'))
+        if ($('.numberCon ul:first-child').css('opacity') == 0) {
+            clearInterval(timerOff);
+        }
+    }
+
+    /*if ($('.numberCon ul:last-child').css('opacity') == 0) {
+        var i = -1;
+        var timerOn = setInterval(fadeOn, 100);
+    }
+    else {
+        //var i = $('.numberCon ul').length
+        var i = 7;
+        var timerOff = setInterval(fadeOff, 100);
+    }*/
 }
